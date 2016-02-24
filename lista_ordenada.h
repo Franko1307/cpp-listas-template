1c4db14161eba69bdf52ca42d7a7b82ada19542c
fran <franciscogonzalez1307@gmail.com>
2016-02-23 19:08:04 -0700

    Mejorando listas

--- lista_ordenada.h
+++ lista_ordenada.h
@@ -7,8 +7,7 @@
 #pragma once
 #include <iostream>
 
-enum Ubicacion{VACIO, INICIO, MITAD, FINAL};
-enum Encontrado{SI,NO};
+enum Ubicacion{INICIO,MITAD};
 
 template <class T> struct mult_caja {
   mult_caja<T> *siguiente;
@@ -18,14 +17,14 @@ template <class T> struct mult_caja {
 template <class T> class list_ord_sin_rep {
 private:
   Ubicacion donde;
-  Encontrado encontrado;
+  bool encontrado;
   mult_caja<T> *principio, *anterior;
-  void buscar(T var);
+  void buscar(T);
 public:
-  list_ord_sin_rep(): principio(NULL), anterior(NULL), donde(VACIO), encontrado(NO){};
+  list_ord_sin_rep(): principio(NULL), anterior(NULL), donde(INICIO), encontrado(false){};
   ~list_ord_sin_rep();
-  bool agregar(T var);
-  bool eliminar(T var);
+  bool agregar(T);
+  bool eliminar(T);
   void pintar();
 };
 /*
@@ -40,8 +39,8 @@ template <class T> list_ord_sin_rep<T>::~list_ord_sin_rep() {
     delete p;
   }
   //Volvemos estos valores nulos también.
-  donde = VACIO;
-  encontrado = NO;
+  donde = INICIO;
+  encontrado = false;
   principio = NULL;
   anterior = NULL;
 }
@@ -51,47 +50,34 @@ template <class T> list_ord_sin_rep<T>::~list_ord_sin_rep() {
  * T var; Es el elemento a buscar en la lista ordenada sin repetición.
  */
 template <class T> void list_ord_sin_rep<T>::buscar(T var) {
-  mult_caja<T> *p;
   //Si esto pasa quiere decir que en la lista no hay nada, por lo cual ni para
-  //que buscar, simplemente decimos que no lo encontramos y ponemos vacio
+  //que buscar, simplemente decimos que no lo encontramos y ponemos inicio
   //y lo regresamos, el anterior es opcional pero pues para asegurar.
-  if (principio == NULL) {
-    encontrado = NO;
-    donde = VACIO;
+  if (!principio) {
+    encontrado = false;
+    donde = INICIO;
     anterior = NULL;
     return;
   }
-  p = principio;
-  //Sino está vacía entonces la recorreremos
+  mult_caja<T> *p = principio;
   while (p) {
-    //El mejor caso, encontramos el valor
-    if (p->var == var){
-      encontrado = SI; //encontrado
-      //Ahora vamos a ver dónde está.
-      if (principio == p) donde = INICIO;
-      else if (p->siguiente == NULL) donde = FINAL;
-      else donde = MITAD;
-      //Y ya terminamos y regresamos.
-      return;
-    }
-    //Esto quiere decir que el valor actual de p es menor que el de var
-    //Lo cual no nos dice nada así que seguimos recorriendo
-    else if (p->var < var){
+    if (p->var < var){
       anterior = p;
       p = p->siguiente;
-    } else {
-      //Pero si pasa esto 'p->var > var' quiere decir que nos pasamos
-      //Y ya no va a estar en el otro lado ya que está ordenada
-      encontrado = NO;
-      //Y buscamos dónde nos quedamos.
+    } else if (p->var == var){ //Caso donde encontramos el valor
+      encontrado = true;
       if (principio == p) donde = INICIO;
-      else if (p->siguiente == NULL) donde = FINAL;
+      else donde = MITAD;
+      return;
+    } else {
+      encontrado = false;
+      if (principio == p) donde = INICIO;
       else donde = MITAD;
       return;
     }
   }
-  encontrado = NO;
-  donde = FINAL;
+  encontrado = false;
+  donde = MITAD;
 }
 /*
  * Función que se encarga de agregar un valor a una lista ordenada sin repetición
@@ -100,28 +86,16 @@ template <class T> void list_ord_sin_rep<T>::buscar(T var) {
  */
 template <class T> bool list_ord_sin_rep<T>::agregar(T var) {
   mult_caja<T> *p;
-  //Utilizamos buscar para que nos asigne las variables
-  //encontrado y donde
   buscar(var);
-  //Si el elemento ya se encuentra en la lista no lo agregamos
-  if (encontrado == SI) return false;
+  if (encontrado) return false;
   p = new mult_caja<T>;
   p->var = var;
-  //Si donde es vacío quiere decir que no había ningún elemento en la lista
-  if (donde == VACIO) {
-    p->siguiente = NULL;
-    principio = p;
-  }
-  else if (donde == INICIO) {
-    p->siguiente = principio;
-    principio = p;
-  //Aquí cambié el código original, quité la parte de final ya que
-  //Aunque no en todos los casos de estructura de datos es así
-  //En este caso, es decir este programa el final y el medio se comportan igual
-  //Y de hecho, de la manera del código original fallaba y así funciona
-  } else {
+  if (donde == MITAD) {
     p->siguiente = anterior->siguiente;
     anterior->siguiente = p;
+  } else {
+    p->siguiente = principio;
+    principio = p;
   }
   return true;
 }
@@ -129,20 +103,15 @@ template <class T> bool list_ord_sin_rep<T>::agregar(T var) {
  * Esta función se encarga de eliminar un elemento de la lista
  */
 template <class T> bool list_ord_sin_rep<T>::eliminar(T var) {
-  mult_caja<T> *p;
   buscar(var);
-  //Buscamos si el elemento está en la lista, si no está devolvemos falso
-  if (encontrado == NO) return false;
-  //Con donde conseguimos la ubicación del elemento
-  if (donde == INICIO) {
-    p = principio;
-    principio = p->siguiente;
-  } else if (donde == FINAL) {
-      p = anterior->siguiente;
-      anterior->siguiente = NULL;
+  mult_caja<T> *p;
+  if (!encontrado) return false;
+  if (donde == MITAD) {
+    p = anterior->siguiente;
+    anterior->siguiente = p->siguiente;
   } else {
-      p = anterior->siguiente;
-      anterior->siguiente = p->siguiente;
+      p = principio;
+      principio = p->siguiente;
   }
   delete p;
   return true;
@@ -152,22 +121,23 @@ template <class T> bool list_ord_sin_rep<T>::eliminar(T var) {
  */
 template <class T> void list_ord_sin_rep<T>::pintar() {
   mult_caja<T> *p = principio;
-  //Vamos recorriendo la lista con p e imprimiendo sus valores.
+  std::cout << "[";
   while (p) {
-    std::cout << p->var << std::endl;
+    std::cout << p->var << ",";
     p = p->siguiente;
   }
+  std::cout << "\b]" << std::endl;
 }
 //**************************************************************************
 //Inicia otra clase, la clase de la lista ordenada con mucha repetición.
 template <class T> class list_ord_con_mu_rep {
 private:
   Ubicacion donde;
-  Encontrado encontrado;
+  bool encontrado;
   mult_caja<T> *principio, *anterior;
   void buscar(T var);
 public:
-  list_ord_con_mu_rep(): donde(VACIO), encontrado(NO), principio(NULL), anterior(NULL){};
+  list_ord_con_mu_rep(): donde(INICIO), encontrado(false), principio(NULL), anterior(NULL){};
   ~list_ord_con_mu_rep();
   bool agregar(T var);
   bool borrar(T var);
@@ -180,43 +150,41 @@ template <class T> list_ord_con_mu_rep<T>::~list_ord_con_mu_rep() {
     principio=p->siguiente;
     delete p;
   }
-  donde = VACIO;
-  encontrado = NO;
+  donde = INICIO;
+  encontrado = false;
   principio = NULL;
   anterior = NULL;
 }
 template <class T> void list_ord_con_mu_rep<T>::buscar(T var) {
-  mult_caja<T> *p;
-  if (principio == NULL) {
-    encontrado = NO;
-    donde = VACIO;
+  if (!principio) {
+    encontrado = false;
+    donde = INICIO;
+    anterior = NULL;
     return;
   }
-  p = principio;
+  mult_caja<T> *p = principio;
   while (p) {
-    if (p->var == var) {
-      encontrado = SI;
-      if (principio == p) donde = INICIO;
-      else if (p->siguiente == NULL) donde = FINAL;
-      else donde = MITAD;
-      return;
-    } else if (p->var < var) {
+    if (p->var < var) {
       anterior = p;
       p = p->siguiente;
-    } else {
-      encontrado = NO;
+    } else if (p->var == var) {
+      encontrado = true;
       if (principio == p) donde = INICIO;
-      else if (p->siguiente == NULL) donde = FINAL;
+      else donde = MITAD;
+      return;
+    } else {
+      encontrado = false;
+      if (principio == p) donde = INICIO;
       else donde = MITAD;
       return;
     }
   }
-  encontrado = NO;
-  donde = FINAL;
+  encontrado = false;
+  donde = MITAD;
 }
 template <class T> bool list_ord_con_mu_rep<T>::agregar(T var) {
   buscar(var);
-  if (encontrado == SI) {
+  if (encontrado) {
     if (donde == INICIO) principio->cantidad++;
     else anterior->siguiente->cantidad++;
     return true;
@@ -225,22 +193,19 @@ template <class T> bool list_ord_con_mu_rep<T>::agregar(T var) {
   p = new mult_caja<T>;
   p->var = var;
   p->cantidad = 1;
-  if (donde == VACIO) {
-    p->siguiente = NULL;
-    principio = p;
-  } else if (donde == INICIO){
-    p->siguiente = principio;
-    principio = p;
-  } else {
+  if (donde == MITAD){
     p->siguiente = anterior->siguiente;
     anterior->siguiente = p;
+  } else {
+    p->siguiente = principio;
+    principio = p;
   }
   return true;
 }
 template <class T> bool list_ord_con_mu_rep<T>::borrar(T var) {
   mult_caja<T> *p;
   buscar(var);
-  if (encontrado == SI){
+  if (encontrado){
     if (donde == INICIO) p = principio;
     else p = anterior->siguiente;
     if (p->cantidad > 1) p->cantidad--;
@@ -255,24 +220,26 @@ template <class T> bool list_ord_con_mu_rep<T>::borrar(T var) {
 }
 template <class T> void list_ord_con_mu_rep<T>::pintar() {
   mult_caja<T> *p = principio;
+  std::cout << "[";
   while (p) {
-    std::cout << p->var << " : " << p->cantidad << std::endl;
+    std::cout <<"{"<< p->var << " : " << p->cantidad << "} ";
     p = p->siguiente;
   }
+  std::cout << "\b]" << std::endl;
 }
 //*********************************************************************
 //Aquí inicia la clase de lista ordenada con poca repetición
 template <class T> class list_ord_con_rep{
 private:
   Ubicacion donde;
-  Encontrado encontrado;
+  bool encontrado;
   mult_caja<T> *principio, *anterior;
-  void buscar(T var);
+  void buscar(T);
 public:
-  list_ord_con_rep(): donde(VACIO),encontrado(NO),principio(NULL),anterior(NULL){};
+  list_ord_con_rep(): donde(INICIO),encontrado(false),principio(NULL),anterior(NULL){};
   ~list_ord_con_rep();
-  bool agregar(T var);
-  bool borrar(T var);
+  bool agregar(T);
+  bool borrar(T);
   void pintar();
 
 };
@@ -283,57 +250,56 @@ template <class T> list_ord_con_rep<T>::~list_ord_con_rep() {
     principio = p->siguiente;
     delete p;
   }
+  encontrado = false;
+  donde = INICIO;
+  principio = anterior = NULL;
 }
 template <class T> void list_ord_con_rep<T>::buscar(T var) {
-  mult_caja<T> *p;
-  if (principio == NULL) {
-    donde = VACIO;
-    encontrado = NO;
+  if (!principio) {
+    donde = INICIO;
+    encontrado = false;
     return;
   }
-  p = principio;
+  mult_caja<T> *p = principio;
   while (p) {
-    if (p->var == var) {
-      encontrado = SI;
-      if (p == principio) donde = INICIO;
-      else if (p->siguiente == NULL) donde = FINAL;
-      else donde = MITAD;
-      return;
-    } else if (p->var < var) {
+    if (p->var < var) {
       anterior = p;
       p = p->siguiente;
-    } else {
-      encontrado = NO;
+    } else if (p->var == var) {
+      encontrado = true;
       if (p == principio) donde = INICIO;
-      else if (p->siguiente == NULL) donde = FINAL;
+      else donde = MITAD;
+      return;
+    } else {
+      encontrado = false;
+      if (p == principio) donde = INICIO;
       else donde = MITAD;
       return;
     }
   }
-  encontrado = NO;
-  donde = FINAL;
+  encontrado = false;
+  donde = MITAD;
 }
 template <class T> bool list_ord_con_rep<T>::agregar(T var) {
   mult_caja<T> *p;
   buscar(var);
   p = new mult_caja<T>;
   p->var = var;
-  if (donde == VACIO) {
-    p->siguiente = NULL;
-    principio = p;
-  } else if (donde == INICIO) {
-    p->siguiente = principio;
-    principio = p;
-  } else {
+  if (donde == MITAD) {
     p->siguiente = anterior->siguiente;
     anterior->siguiente = p;
+  } else {
+    p->siguiente = principio;
+    principio = p;
   }
   return true;
 }
 template <class T> void list_ord_con_rep<T>::pintar() {
   mult_caja<T> *p = principio;
+  std::cout << "[";
   while (p) {
-    std::cout << p->var << std::endl;
+    std::cout << p->var << ",";
     p = p->siguiente;
   }
+  std::cout << "\b]" << std::endl;
 }
